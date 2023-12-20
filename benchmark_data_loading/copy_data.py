26650ecb43efc75a882c877b3c81406887648344
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2023-12-20 08:13:24 -0700

    Save boilerplate code for psycopg COPY

--- benchmark_data_loading/copy_data.py
+++ benchmark_data_loading/copy_data.py
@@ -47,3 +47,25 @@ Parallel(n_jobs=8)(
 
 t2 = time.perf_counter()
 print(f"Inserted {100 * 1038240:,} rows in {t2 - t1:.3f} seconds = {int(100*1038240 / (t2 - t1)):,} inserts/second")
+
+
+# with psycopg.connect("user=postgres password=oceans host=localhost port=5432 dbname=climarisk") as conn:
+#     with conn.cursor() as cur:
+
+#         ds = xr.open_dataset("/storage3/alir/era5/2t/e5.oper.an.sfc.128_167_2t.ll025sc.1995030100_1995033123.nc")
+#         ds.VAR_2T.load()
+
+#         for n, t in enumerate(tqdm(ds.time.values, desc="time", position=0)):
+#             with cur.copy("COPY weather (latitude, longitude, time, temperature) FROM STDIN") as copy:
+#                 for i, lat in enumerate(tqdm(ds.latitude.values, desc="latitude", position=1, leave=False)):
+#                     for j, lon in enumerate(ds.longitude.values):
+#                         temp = ds.VAR_2T.values[n, i, j] - 273.15
+#                         record = (lat, lon, pd.Timestamp(t), temp)
+#                         copy.write_row(record)
+
+#             # for i, lat in enumerate(tqdm(ds.latitude.values, desc="latitude", position=1, leave=False)):
+#             #     for j, lon in enumerate(ds.longitude.values):
+#             #         temp = ds.VAR_2T.values[n, i, j] - 273.15
+#             #         cur.execute(f"INSERT INTO weather (latitude, longitude, time, temperature) VALUES ({lat}, {lon}, '{t}', {temp})")
+
+#             conn.commit()
