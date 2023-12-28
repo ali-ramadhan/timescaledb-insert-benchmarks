bc3a0f071093fe7332b4914f4221bdac35f008b3
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2023-12-27 18:52:38 -0700

    Batched insert should be multi-insert

--- benchmark_data_loading/run_multi_insert_benchmarks.bash
+++ benchmark_data_loading/run_multi_insert_benchmarks.bash
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+source .env
+
 methods=("pandas" "psycopg3" "sqlalchemy")
+runs=10
 
 for method in "${methods[@]}"; do
-    for i in {1..10}; do
+    for (( i=1; i<=$runs; i++ )); do
         set -x
+
+        docker-compose up --detach
+
+        while ! pg_isready -h "$POSTGRES_HOST" -U "$POSTGRES_USER"; do
+            sleep 2
+        done
 
         poetry run python create_table.py \
             --drop-table &&
@@ -21,20 +30,22 @@ for method in "${methods[@]}"; do
             --num-rows 50000 \
             --method $method
 
-        set +x
+        docker-compose down
+
+        { set +x; } 2>/dev/null
     done
 done
 
-merged_csv="benchmarks_batch_insert.csv"
+merged_csv="benchmarks_multi_insert.csv"
 
 if [ -f "$merged_csv" ]; then
     rm "$merged_csv"
 fi
 
 echo "method,num_rows,seconds,rate,units,hypertable" > "$merged_csv"
-awk 'NR > 1 {print $0",false"}' benchmarks_batch_insert_nohypertable.csv >> "$merged_csv"
-awk 'NR > 1 {print $0",true"}' benchmarks_batch_insert_hypertable.csv >> "$merged_csv"
-rm benchmarks_batch_insert_nohypertable.csv
-rm benchmarks_batch_insert_hypertable.csv
+awk 'NR > 1 {print $0",false"}' benchmarks_multi_insert_nohypertable.csv >> "$merged_csv"
+awk 'NR > 1 {print $0",true"}' benchmarks_multi_insert_hypertable.csv >> "$merged_csv"
+rm benchmarks_multi_insert_nohypertable.csv
+rm benchmarks_multi_insert_hypertable.csv
 
 poetry run python plot_insert_benchmarks.py --benchmarks-file "$merged_csv"
