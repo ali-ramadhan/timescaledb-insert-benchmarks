d98f30924d4fb443d7711a19b3cd459e22249da0
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2023-12-31 22:26:30 -0500

    Preliminary at scale benchmark plots

--- benchmark_data_loading/plot_copy_at_scale_benchmark.py
+++ benchmark_data_loading/plot_copy_at_scale_benchmark.py
@@ -4,12 +4,24 @@ import matplotlib.pyplot as plt
 
 def main(args):
     df = pd.read_csv("benchmarks_copy_at_scale.csv")
-    
-    rows_inserted = df.query("hypertable == False")["num_rows"].cumsum()
-    insert_rate_rt = df.query("hypertable == False")["rate_copy"]
-    insert_rate_ht = df.query("hypertable == True")["rate_copy"]
 
-    plt.plot(rows_inserted, insert_rate_rt, label="Regular table")
-    plt.plot(rows_inserted, insert_rate_ht, label="Hypertable")
+    def plot(method, rate, hypertable, label):
+    	rows_inserted = df.query(f"method == '{method}' and hypertable == {hypertable}")["num_rows"].cumsum()
+    	insert_rate = df.query(f"method == '{method}' and hypertable == {hypertable}")[rate]
+    	insert_rate_smoothed = insert_rate.rolling(10).mean()
+
+    	plt.scatter(rows_inserted / 1e6, insert_rate, marker=".", alpha=0.5, label=label)
+    	plt.plot(rows_inserted / 1e6, insert_rate_smoothed, label=label)
+
+    plot("psycopg3", "rate_copy", False, label="psycopg3 + regular table")
+    plot("psycopg3", "rate_copy", True, label="psycopg3 + hypertable")
+    plot("copy_csv", "rate_copy", False, label="COPY + regular table")
+
+    plt.xlabel("Rows inserted (millions)")
+    plt.ylabel("Insert rate (rows per second)")
+    plt.legend()
     
-    plt.savefig("benchmarks_copy_at_scale.png", dpi=100, transparent=False)
+    plt.savefig("benchmarks_copy_at_scale.png", dpi=200, transparent=False)
+
+if __name__ == "__main__":
+    main(None)
