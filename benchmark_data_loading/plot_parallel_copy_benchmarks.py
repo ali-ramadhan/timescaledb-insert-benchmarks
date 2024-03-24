d7ad7b2f437fe5791b34e684d5e1e2afeadbbc4e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2024-03-24 03:33:24 -0400

    Better parallel copy plot

--- benchmark_data_loading/plot_parallel_copy_benchmarks.py
+++ benchmark_data_loading/plot_parallel_copy_benchmarks.py
@@ -1,25 +1,33 @@
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def main():
     df = pd.read_csv("benchmarks_parallel_copy.csv")
 
-    def plot(method, table_type, label):
+    def plot(ax, method, table_type, label):
         num_workers = df.query(f"method == '{method}' and table_type == '{table_type}'")["workers"]
         insert_rate = df.query(f"method == '{method}' and table_type == '{table_type}'")["rate"]
-        plt.plot(num_workers, insert_rate, marker='o', label=label)
+        ax.plot(num_workers, insert_rate, marker='o', label=label)
 
-    plot("psycopg3", "regular", label="psycopg3 (regular table)")
-    plot("psycopg3", "hyper", label="psycopg3 (hypertable)")
-    plot("copy_csv", "regular", label="COPY CSV (regular table)")
-    plot("copy_csv", "hyper", label="COPY CSV (hypertable)")
+    fig, ax = plt.subplots(figsize=(8, 6))
 
-    plt.xlabel("Workers")
-    plt.ylabel("Insert rate (rows per second)")
-    plt.legend()
+    plot(ax, "psycopg3", "regular", label="psycopg3 (regular table)")
+    plot(ax, "psycopg3", "hyper", label="psycopg3 (hypertable)")
+    plot(ax, "copy_csv", "regular", label="COPY CSV (regular table)")
+    plot(ax, "copy_csv", "hyper", label="COPY CSV (hypertable)")
+
+    def to_k(val, pos):
+        return f"{val/1000:.0f}k"
+
+    ax.yaxis.set_major_formatter(mpl.ticker.FuncFormatter(to_k))
+
+    ax.set_xlabel("Workers")
+    ax.set_ylabel("Insert rate (rows per second)")
+    ax.legend(frameon=False, ncol=2, loc="upper center", bbox_to_anchor=(0.5, 1.15))
     
-    plt.savefig("benchmarks_parallel_copy.png", dpi=200, transparent=False)
+    fig.savefig("benchmarks_parallel_copy.png", dpi=200, transparent=False)
 
 if __name__ == "__main__":
     main()
