fb4a94618d2a08609fb66788085ef9c130e07477
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2024-03-27 11:27:49 -0400

    Separate script for plotting tool benchmarks

---
+++ benchmark_data_loading/plot_tool_benchmarks.py
@@ -0,0 +1,105 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Make bar plots of insert benchmarks."
+    )
+
+    parser.add_argument(
+        "--benchmarks-file",
+        type=str,
+        help="Filepath to a benchmarks CSV file.",
+        required=True
+    )
+    
+    return parser.parse_args()
+
+def p10(x):
+    return np.percentile(x, 10)
+
+def p90(x):
+    return np.percentile(x, 90)
+
+def num_to_kM(val, pos):
+    if val < 1e3:
+        return f"{val:.0f}"
+    if 1e3 <= val < 1e6:
+        return f"{val/1e3:.0f}k"
+    elif 1e6 <= val < 1e9:
+        return f"{val/1e6:.0f}M"
+
+def main(args):
+    df = pd.read_csv(args.benchmarks_file)
+
+    pt = pd.pivot_table(df,
+        values="rate",
+        index=["method", "table_type"],
+        aggfunc=["median", p10, p90]
+    )
+
+    methods = pt.index.get_level_values("method").unique()
+
+    def get(var, agg, table_type):
+        return pt[agg][var].xs(table_type, level="table_type").reindex(methods)
+
+    def ranges(var, table_type):
+        return [
+            get(var, "median", table_type) - get(var, "p10", table_type),
+            get(var, "p90", table_type) - get(var, "median", table_type)            
+        ]
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    x = np.arange(len(methods))
+    width = 0.2  # the width of the bars
+
+    ax.bar(
+        x - 1.5*width,
+        get("rate", "median", "regular"),
+        width,
+        yerr=ranges("rate", "regular"),
+        capsize=5,
+        label="Regular table"
+    )
+
+    ax.bar(
+        x - 0.5*width,
+        get("rate", "median", "hyper"),
+        width,
+        yerr=ranges("rate", "hyper"),
+        capsize=5,
+        label="Hypertable"
+    )
+
+    ax.bar(
+        x + 0.5*width,
+        [224941.351025535, 109085.120946],
+        width,
+        label="Sustained rate (hypertable)"
+    )
+
+    ax.bar(
+        x + 1.5*width,
+        [171050.81343950325, 94462.51879792413],
+        width,
+        label="Sustained rate including\noverhead (hypertable)"
+    )
+
+    ax.yaxis.set_major_formatter(mpl.ticker.FuncFormatter(num_to_kM))
+
+    ax.set_ylabel("Insert rate (rows per second)")
+    ax.set_xticks(x)
+    ax.set_xticklabels(methods)
+    ax.legend(frameon=False)
+
+    output_filename = Path(args.benchmarks_file).with_suffix(".png")
+    fig.savefig(output_filename, dpi=200, transparent=False, bbox_inches="tight")
+
+if __name__ == "__main__":
+    main(parse_args())
