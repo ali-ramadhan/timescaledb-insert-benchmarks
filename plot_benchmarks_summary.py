8c70700abde000129946b2b2c248fb9bce194d3f
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2024-03-31 13:01:18 -0400

    Fix summarized benchmarks

--- plot_benchmarks_summary.py
+++ plot_benchmarks_summary.py
@@ -12,8 +12,8 @@ num_rows_31days = 31 * 24 * num_rows_1hour
 t_csv_31days = 1068.49 # 16 workers (faster than 32)
 t_csv_256hours = (256 / (31 * 24)) * t_csv_31days
 
-insert_rate_pg_bulkload = time_to_insert_rate(1186.21, t_csv_256hours, num_rows_256hours)
-insert_rate_pg_bulkload_parallel = time_to_insert_rate(849.89, t_csv_256hours, num_rows_256hours)
+insert_rate_pg_bulkload = time_to_insert_rate(1542.76, t_csv_256hours, num_rows_256hours)
+insert_rate_pg_bulkload_parallel = time_to_insert_rate(1316.53, t_csv_256hours, num_rows_256hours)
 insert_rate_pg_bulkload_parallel_nofsync = time_to_insert_rate(2537.03, t_csv_31days, num_rows_31days)
 insert_rate_tpc_1worker = time_to_insert_rate(2446.05018252, t_csv_256hours, num_rows_256hours)
 insert_rate_tpc_32workers = time_to_insert_rate(1046.690627062, t_csv_256hours, num_rows_256hours)
@@ -28,13 +28,11 @@ benchmarks = {
     "copy\npsycopg3": 85_000,
     "tpc": insert_rate_tpc_1worker,
     "pgb": insert_rate_pg_bulkload,
-    "tpc\n32W": insert_rate_tpc_32workers,
     "pgb\nparallel": insert_rate_pg_bulkload_parallel,
-    "copy\npsycopg3\n32W": 241_641,
+    "tpc\n32W": insert_rate_tpc_32workers,
+    "copy\npsycopg3\n32W": 248_706,
     "pgb\nparallel\nfsync off": insert_rate_pg_bulkload_parallel_nofsync,
-    # "tpc\n16W\nfsync off": insert_rate_tpc_16workers_nofsync,
     "tpc\n32W\nfsync off": insert_rate_tpc_32workers_nofsync,
-    # "copy\npsycopg3\n16W\nfsync off": 417_165,
     "copy\npsycopg3\n32W\nfsync off": 462_117,
 }
 
